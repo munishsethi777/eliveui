d73540c093e3ce2f2e930d9fe10ac14b15bbba9f
bgaheer <baljeetgaheer@gmail.com>
2015-12-26 12:54:44 +0530

    d

---
+++ _InspiniaInclude.php
@@ -0,0 +1,48 @@
+<?session_start(); 
+$managerSession = $_SESSION["managerSession"];
+$protocol  = empty($_SERVER['HTTPS']) ? 'http://' : 'https://';
+$path=$protocol.getenv('HTTP_HOST')."/eliveui/";?>
+
+<!-- Mainly CSS --> 
+    <link href="<?echo$path?>inspinia/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?echo$path?>inspinia/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="<?echo$path?>inspinia/css/animate.css" rel="stylesheet">
+    <link href="<?echo$path?>inspinia/css/style.css" rel="stylesheet">
+<!-- Toastr style -->
+    <link href="<?echo$path?>inspinia/css/plugins/toastr/toastr.min.css" rel="stylesheet">
+
+<!-- Mainly scripts -->
+    <script src="<?echo$path?>inspinia/js/jquery-2.1.1.js"></script>
+    <script src="<?echo$path?>inspinia/js/bootstrap.min.js"></script>
+    <script src="<?echo$path?>inspinia/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="<?echo$path?>inspinia/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+    <script src="<?echo$path?>inspinia/js/inspinia.js"></script>
+    <script src="<?echo$path?>inspinia/js/plugins/pace/pace.min.js"></script>
+<!-- Toastr script -->
+    <script src="<?echo$path?>inspinia/js/plugins/toastr/toastr.min.js"></script>
+ <!-- Ladda button -->
+    <link rel="stylesheet" href="<?echo$path?>css/ladda-themeless.min.css">
+    <script src="<?echo$path?>js/jquery.form.min.js"></script>
+    <script src="<?echo$path?>js/spin.min.js"></script>
+    <script src="<?echo$path?>js/scripts/ladda.min.js"></script>
+    
+    <script language="javascript">
+        function showNotification($msg){
+            toastr.options = {
+                  "closeButton": true,
+                  "debug": false,
+                  "progressBar": true,
+                  "positionClass": "toast-top-center",
+                  "onclick": null,
+                  "showDuration": "400",
+                  "hideDuration": "1000",
+                  "timeOut": "7000",
+                  "extendedTimeOut": "1000",
+                  "showEasing": "swing",
+                  "hideEasing": "linear",
+                  "showMethod": "fadeIn",
+                  "hideMethod": "fadeOut"
+            }
+            toastr.error($msg);
+        }
+    </script>
